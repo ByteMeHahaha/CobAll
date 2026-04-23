eb981a6897fe85d0077885c435766372d8f21e75
Ethan <bbruhingson@gmail.com>
2026-04-23 06:02:22 +0200

    docs: Add comments to `main.cob` source code

--- src/main.cob
+++ src/main.cob
@@ -17,12 +17,24 @@ DATA DIVISION.
       01 DLF-Debug-Line PIC X(80).
 
   WORKING-STORAGE SECTION.
+    *> Log file status
     01 WS-Debug-File-Status PIC XX.
+      88 Opened-Successfully VALUE "00".
+
+    *> Log levels for debug file
+    01 WS-Log-Levels.
+      05 WS-Log-Debug PIC X(3) VALUE "DBG".
+      05 WS-Log-Info PIC X(3) VALUE "INF".
+      05 WS-Log-Warn PIC X(4) VALUE "WARN".
+      05 WS-Log-Error PIC X(3) VALUE "ERR".
+
+    *> Temp storage for writing to the debug file
     01 WS-Debug-File-Writing.
       05 WS-Debug-Line PIC X(80).
       05 WS-Debug-Message PIC X(40).
       05 WS-Debug-Level PIC A(4).
 
+    *> Today's date in ISO Format, used in the log file
     01 WS-Date.
       05 WS-Year PIC 9999.
       05 FILLER PIC X VALUE "-".
@@ -30,6 +42,7 @@ DATA DIVISION.
       05 FILLER PIC X VALUE "-".
       05 WS-Day PIC 99.
 
+    *> Current time, used for the log file
     01 WS-Time.
       05 WS-Hour PIC 99.
       05 FILLER PIC X VALUE ":".
@@ -38,7 +51,7 @@ DATA DIVISION.
     *> User-provided values for use
     *> throughout the program
     01 WS-User-Input.
-      05 WS-Main-Menu-Choice PIC 9 VALUE "0".
+      05 WS-Main-Menu-Choice PIC 9 VALUE 0.
 
   *> Menus and other screens
   SCREEN SECTION.
@@ -47,18 +60,23 @@ DATA DIVISION.
 
 PROCEDURE DIVISION.
   MainCode.
+    *> Log the program start to the debug file
     MOVE "INF" TO WS-Debug-Level.
     MOVE "Program Started" TO WS-Debug-Message.
     PERFORM WriteDebugMessage.
 
+    *> Show the main menu screen
     DISPLAY SC-Main-Menu.
 
+    *> Write a debug log for the menu being shown
     MOVE "DBG" TO WS-Debug-Level.
     MOVE "Main Menu Screen Shown" TO WS-Debug-Message.
     PERFORM WriteDebugMessage.
 
+    *> Accept input for the main menu screen
     ACCEPT SC-Main-Menu.
 
+    *> Write the chosen menu item to the log file
     MOVE "INF" TO WS-Debug-Level.
     STRING
       "User picked menu item: " DELIMITED BY SIZE
@@ -68,29 +86,35 @@ PROCEDURE DIVISION.
     END-STRING.
     PERFORM WriteDebugMessage.
 
+    *> Display the debug screen
     DISPLAY SC-Debug-Screen.
 
+    *> Write to the debug log
     MOVE "DBG" TO WS-Debug-Level.
     MOVE "Debug screen shown" TO WS-Debug-Message.
     PERFORM WriteDebugMessage.
 
+    *> "Press enter to exit"
     ACCEPT OMITTED.
 
+    *> Write the app being closed to the debug log
     MOVE "INF" TO WS-Debug-Level.
     MOVE "Program Closing" TO WS-Debug-Message.
     PERFORM WriteDebugMessage.
 
+    *> Close the app
     STOP RUN.
 
   *> Writes debug info to the log file
   WriteDebugMessage.
+    *> Initialise the date variables for the new log file entry
     PERFORM InitialiseDate.
 
-    *> Attempt to open the debug file to append to it
+    *> Open the debug file to append to it
     OPEN EXTEND Debug-Log-File.
 
     *> If it can't open successfully
-    IF WS-Debug-File-Status NOT EQUAL "00" THEN
+    IF NOT Opened-Successfully THEN
       *> Store a message for the new log file
       MOVE "WARN" TO WS-Debug-Level
       MOVE "Debug File Recreated" TO WS-Debug-Message
@@ -98,7 +122,7 @@ PROCEDURE DIVISION.
       OPEN OUTPUT Debug-Log-File
     END-IF.
 
-    *> Initialise the debug log line
+    *> Initialise the temporary debug log variable
     MOVE SPACES TO WS-Debug-Line.
 
     *> Build the debug log line
@@ -111,7 +135,7 @@ PROCEDURE DIVISION.
       "] " DELIMITED BY SIZE
       FUNCTION TRIM(WS-Debug-Message) DELIMITED BY SIZE
 
-      *> e.g.: 2026-04-20, 10:24 [DEBUG] Test
+      *> e.g.: 2026-04-20, 10:24 [DBG] Test
       INTO WS-Debug-Line
     END-STRING.
 
@@ -123,15 +147,18 @@ PROCEDURE DIVISION.
     *> Initialise the debug variables
     PERFORM InitialiseDebugInfo.
 
-  *> Initialises the date for the log file.
+  *> Initialises the date variables for the log file.
   InitialiseDate.
+    *> Store the current date in the respective variables
     MOVE FUNCTION CURRENT-DATE(1:4) TO WS-Year.
     MOVE FUNCTION CURRENT-DATE(5:2) TO WS-Month.
     MOVE FUNCTION CURRENT-DATE(7:2) TO WS-Day.
 
+    *> Store the current time in the respective variables
     MOVE FUNCTION CURRENT-DATE(9:2) TO WS-Hour.
     MOVE FUNCTION CURRENT-DATE(11:2) TO WS-Minute.
 
+  *> Initialise the debug log's temporary variables
   InitialiseDebugInfo.
     MOVE SPACES TO WS-Debug-Level.
     MOVE SPACES TO WS-Debug-Message.
